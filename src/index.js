39e9d30561b639f16299b9729451a885db8f86dd
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-08-21 17:19:40 +0200

    Add various tests to check the functionality of the binary search tree

--- src/index.js
+++ src/index.js
@@ -2,30 +2,34 @@ import "./styles.css";
 import { Tree } from "./tree.js";
 import { prettyPrint } from "./utilities.js";
 
-const testArray = [5, 4, 2, 8, 6, 4];
-const testTree = new Tree(testArray);
-
-prettyPrint(testTree.root);
-
-testTree.insert(14);
-testTree.insert(1);
-
-prettyPrint(testTree.root);
-
-testTree.remove(8);
-
-prettyPrint(testTree.root);
-
-console.log(testTree.find(4));
-
-function double(value) {
-  return value * 2;
+function randomNumbers(size) {
+  randomNumbers = [];
+  for (let i = 0; i < size; i++) {
+    randomNumbers.push(Math.round(Math.random() * 100));
+  }
+  return randomNumbers;
 }
+
+const testTree = new Tree(randomNumbers(20));
+
+console.log(testTree.isBalanced());
+
 console.log(testTree.levelOrder((value) => value));
+console.log(testTree.inOrder((value) => value));
+console.log(testTree.preOrder((value) => value));
+console.log(testTree.postOrder((value) => value));
 
-console.log(testTree.inOrder(double));
-console.log(testTree.preOrder(double));
-console.log(testTree.postOrder(double));
+testTree.insert(432);
+testTree.insert(291);
+testTree.insert(675);
 
-console.log(testTree.height());
-console.log(testTree.depth(1));
+console.log(testTree.isBalanced());
+
+testTree.rebalance();
+
+console.log(testTree.isBalanced());
+
+console.log(testTree.levelOrder((value) => value));
+console.log(testTree.inOrder((value) => value));
+console.log(testTree.preOrder((value) => value));
+console.log(testTree.postOrder((value) => value));
